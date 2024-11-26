daa4fdcebe51fc455e66e6d49810bf173a93c143
Mahdi <Shah.Mahdi@hotmail.com>
2024-11-26 22:59:30 +0000

    Improvements and more functionality for robot class

--- Robot.py
+++ Robot.py
@@ -1,7 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from enum import Enum
 from Trajectories import QuinticPolynomialTrajectory
+
+class Joints(Enum):
+    BASE = 0
+    LIFT = 1
+    ELBOW = 2
+    WRIST = 3
+    GRIPPER = 4
+
 
 class Robot:
     def __init__(self,
@@ -9,22 +17,14 @@ class Robot:
                  joint_limits=None,
                  joint_max_vels=None,
                  joint_max_accs=None):
-        # Robot has swing (base, J0),  lift (vertical Z axis, J1), elbow (J2), wrist (J3),
-        # Gripper bool (end effector, J4)
-        # Implement joint max speeds and accelerations
-        # Implement trajectories
-        # Base has 4 DOF - mounted on a vehicle with X, Y, Z coordinates and Z rotation
-        # Move the origin while maintaining end effector position
         self.arm_link_lengths = arm_link_lengths
         self.n_arm_links = len(arm_link_lengths)
         self.n_joints = 5
         self.joint_states = [0.0] * self.n_joints
-        self.gripper_vertical_offset = 0.5  # 0.5m below the lift and J1 origin
-        self.base_origin = [0, 0, 0]
+        self.wrist_vertical_offset = 0.5 # 0.2m below the elbow
+        self.gripper_vertical_offset = 0.2  # 0.5m below the lift and J1 origin
+        self.base_position = [0.0, 0.0, 0.0]
         self.trajectory_generator = QuinticPolynomialTrajectory()
-
-
-    # TODO wrap the angles to pi and -pi
     # TODO implement joint limits in the config
 
     def generate_trajectory(self, target_joint_states, control_type='fk'):
@@ -43,25 +43,24 @@ class Robot:
         if len(target_joint_states) > self.n_joints:
             raise ValueError(f"Too many joint states ({len(target_joint_states)} for robot with {self.n_joints} joints")
 
-        a = 1
         for joint_idx, target_state in enumerate(target_joint_states):
             self.joint_states[joint_idx] = target_state
             # TODO implement the trajectories
 
     def move_inverse_kinematics(self, target_position: tuple[float, float, float], target_orientation: float=None):
         # TODO implement trajectories
-        theta_1, theta_2, theta_3 = self.calculate_inverse_kinematics(target_position, target_orientation)
-        self.joint_states[0] = theta_1
-        self.joint_states[1] = target_position[2] + self.gripper_vertical_offset
-        self.joint_states[2] = theta_2
-        self.joint_states[3] = theta_3
+        theta_1, theta_2, theta_3 = self.calculate_inverse_kinematics(target_position[0:2], target_orientation)
+        self.joint_states[Joints.BASE.value] = theta_1
+        self.joint_states[Joints.LIFT.value] = target_position[2] + self.wrist_vertical_offset + self.gripper_vertical_offset
+        self.joint_states[Joints.ELBOW.value] = theta_2
+        self.joint_states[Joints.WRIST.value] = theta_3
 
-    def calculate_inverse_kinematics(self, target_position: tuple[float, float, float], target_orientation: float=None):
+    def calculate_inverse_kinematics(self, target_position: tuple[float, float], target_orientation: float=None):
         # Check target validity
-        # TODO allow non origin based commands
-        target_distance = np.linalg.norm(target_position)
+        relative_target = np.subtract(target_position, self.base_position[0:2])
+        target_distance = np.linalg.norm(relative_target)
         if target_distance > sum(self.arm_link_lengths):
-            # TODO check if target too clsoe too?
+            # TODO check if target is too close to base
             raise ValueError(f"Invalid IK target {target_position} with link lengths {self.arm_link_lengths}")
 
         # Now set the base and elbow joints to set the wrist centre
@@ -70,12 +69,10 @@ class Robot:
         if target_orientation is None:
             target_orientation = np.pi/2
 
-        wrist_centre_point = [target_position[0] - (wrist_length * np.cos(target_orientation)),
-                              target_position[1] - (wrist_length * np.sin(target_orientation)),
-                              target_position[2]]
-        wrist_centre_distance = np.linalg.norm([wrist_centre_point[0], wrist_centre_point[1]])
+        wrist_centre_point = [relative_target[0] - (wrist_length * np.cos(target_orientation)),
+                              relative_target[1] - (wrist_length * np.sin(target_orientation))]
+        wrist_centre_distance = np.linalg.norm(wrist_centre_point)
 
-        print(f"Wrist centre: {wrist_centre_point}")
         # wrist centre position kinematics:
         # using cosine rule
         cos_theta_2 = (wrist_centre_distance ** 2 - self.arm_link_lengths[0] ** 2 - self.arm_link_lengths[1] ** 2) / (2 * self.arm_link_lengths[0] * self.arm_link_lengths[1])
@@ -88,46 +85,104 @@ class Robot:
         theta_3 = target_orientation - (theta_1 + theta_2)
 
         # base, elbow, wrist
-        return [theta_1, theta_2, theta_3]
+        return theta_1, theta_2, theta_3
 
-    def move_base(self, new_position: tuple[float, float, float], maintain_ee: bool=True):
-        pass
+    def get_joint_positions(self):
+        base = self.base_position
+        vertical_lift = [self.base_position[0], self.base_position[1], self.joint_states[Joints.LIFT.value]]
+        elbow = [
+            self.base_position[0] + (self.arm_link_lengths[0] * np.cos(self.joint_states[Joints.BASE.value])),
+            self.base_position[1] + (self.arm_link_lengths[0] * np.sin(self.joint_states[Joints.BASE.value])),
+            self.joint_states[Joints.LIFT.value]
+        ]
+
+        wrist = [
+            elbow[0] + (self.arm_link_lengths[1] * np.cos(self.joint_states[Joints.BASE.value] + self.joint_states[Joints.ELBOW.value])),
+            elbow[1] + (self.arm_link_lengths[1] * np.sin(self.joint_states[Joints.BASE.value] + self.joint_states[Joints.ELBOW.value])),
+            (self.joint_states[Joints.LIFT.value] - self.wrist_vertical_offset)
+        ]
+
+        ee = [
+            wrist[0] + self.arm_link_lengths[2] * np.cos(
+                self.joint_states[Joints.BASE.value] + self.joint_states[Joints.ELBOW.value] + self.joint_states[Joints.WRIST.value]
+            ),
+            wrist[1] + self.arm_link_lengths[2] * np.sin(
+                self.joint_states[Joints.BASE.value] + self.joint_states[Joints.ELBOW.value] + self.joint_states[Joints.WRIST.value]
+            ),
+            (self.joint_states[Joints.LIFT.value] - self.wrist_vertical_offset - self.gripper_vertical_offset)
+        ]
+
+        return base, vertical_lift, elbow, wrist, ee
+
+    def get_arm_joint_positions(self):
+        *_, elbow, wrist, ee = self.get_joint_positions()
+        return elbow, wrist, ee
+
+    def get_joint_states(self):
+        return self.joint_states
+
+
+    def get_ee_position(self):
+        # Get ee position relative to the base
+        *_, ee = self.get_arm_joint_positions()
+        return ee
+
+
+    def move_base(self, new_position: tuple[float, float], maintain_ee: bool=True):
+        # Assume base only moves in a planar motion
+        if maintain_ee:
+            ee_ik_target = self.get_ee_position()
+            target_orientation = self.joint_states[0] + self.joint_states[2] + self.joint_states[3]
+        self.base_position[0] = new_position[0]
+        self.base_position[1] = new_position[1]
+        if maintain_ee:
+            self.move_inverse_kinematics(ee_ik_target, target_orientation)
+
 
     def actuate_gripper(self, desired_state: int):
         # 0 = open
         # 1 = closed
         self.joint_states[-1] = desired_state
 
+    def check_lengths(self):
+        elbow, wrist, ee = self.get_arm_joint_positions()
+        base_length = np.linalg.norm(np.subtract(self.base_position[0:2], elbow[0:2]))
+        elbow_length = np.linalg.norm(np.subtract(elbow[0:2], wrist[0:2]))
+        wrist_length = np.linalg.norm(np.subtract(wrist[0:2], ee[0:2]))
+
+        print(f"Base length is: {base_length}")
+        print(f"elbow length is: {elbow_length}")
+        print(f"wrist length is: {wrist_length}")
 
     def plot_state(self):
         fig, ax = plt.subplots()
-        base = plt.Circle((0.0, 0.0), 0.1)
+        base = plt.Circle((self.base_position[0], self.base_position[1]), 0.1)
         ax.add_artist(base)
 
-        j2_position = [self.arm_link_lengths[0] * np.cos(self.joint_states[0]), self.arm_link_lengths[0] * np.sin(self.joint_states[0])]
-        j3_position = [j2_position[0] + self.arm_link_lengths[1] * np.cos(self.joint_states[0] + self.joint_states[2]),
-                       j2_position[1] + self.arm_link_lengths[1] * np.sin(self.joint_states[0] + self.joint_states[2]),]
-        j4_position = [j3_position[0] + self.arm_link_lengths[2] * np.cos(self.joint_states[0] + self.joint_states[2] + self.joint_states[3]),
-                       j3_position[1] + self.arm_link_lengths[2] * np.sin(self.joint_states[0] + self.joint_states[2] + self.joint_states[3])]
+        elbow, wrist, ee = self.get_arm_joint_positions()
 
-        print(f"J2 position (Elbow): {j2_position}")
-        print(f"J3 position (Wrist): {j3_position}")
-        print(f"J4 position (End Effector): {j4_position}")
+        print(f"J2 position (Elbow): {elbow}")
+        print(f"J3 position (Wrist): {wrist}")
+        print(f"J4 position (End Effector): {ee}")
 
-        plt.plot([0, j2_position[0]], [0, j2_position[1]], 'bo-', label='Base Offset (L1)')  # L1
-        plt.plot([j2_position[0], j3_position[0]], [j2_position[1], j3_position[1]], 'go-', label='Elbow (L2)')  # L2
-        plt.plot([j3_position[0], j4_position[0]], [j3_position[1], j4_position[1]], 'ro-', label='Wrist (L3)')  # L3
+        self.check_lengths()
+
+        plt.plot([self.base_position[0], elbow[0]], [self.base_position[1], elbow[1]], 'bo-', label='Base Offset (L1)')  # L1
+        plt.plot([elbow[0], wrist[0]], [elbow[1], wrist[1]], 'go-', label='Elbow (L2)')  # L2
+        plt.plot([wrist[0], ee[0]], [wrist[1], ee[1]], 'ro-', label='Wrist (L3)')  # L3
         plt.xlim(-0.5, 0.5)
         plt.ylim(-0.5, 0.5)
-        plt.axis('square')
+        ax.axis('equal')
+        plt.tight_layout()
         plt.legend()
         plt.show()
 
 
-
 if __name__ == '__main__':
-    robot = Robot([0.1, 0.3, 0.1])
-    # robot.move_forward_kinematics([0.0,0.0,np.pi/2,np.pi/2,0.0])
-    robot.move_inverse_kinematics((0.3, 0.3, 0.0))
+    robot = Robot([0.3, 0.6, 0.2])
+    robot.move_inverse_kinematics((0.6, 0.2, 0.8))
+    # robot.move_forward_kinematics([0.0, 0.0, np.pi/2, np.pi/2, 0.0])
+    robot.plot_state()
+    robot.move_base([0.2, 0.0], True)
     robot.plot_state()
 
