c6be3e8bf70ff8bc82f2b533fd7151ba27f5c714
Mahdi <Shah.Mahdi@hotmail.com>
2024-12-02 01:31:23 +0000

    Adding type hints, function annotations, and docstrings for documentation

--- backend/Robot.py
+++ backend/Robot.py
@@ -13,11 +13,11 @@ class Joints(Enum):
 
 class Robot:
     def __init__(self,
-                 arm_link_lengths,
-                 joint_max_vels=None,
-                 joint_max_accs=None,
-                 base_max_vel=None,
-                 base_max_acc=None):
+                 arm_link_lengths: list[float],
+                 joint_max_vels: list[float]=None,
+                 joint_max_accs: list[float]=None,
+                 base_max_vel: float=None,
+                 base_max_acc: float=None):
         self.arm_link_lengths = arm_link_lengths
         self.n_arm_links = len(arm_link_lengths)
         self.n_joints = 4  # Base, lift, elbow, wrist
@@ -31,10 +31,18 @@ class Robot:
         self.base_position = [0.0, 0.0, 0.0]
         self.trajectory_points = {"base": [], "joints": []}
         self.trajectory_generator = QuinticPolynomialTrajectory()
+        """Initialises the Robot object based on the starting parameters.
+        Args:
+            arm_link_lengths: Link lengths for the robot. Should be a list of 3 - Base link, Elbow, Wrist
+            joint_max_vels: Velocity limits for each joint. List of 4 values - Base Joint, Lift, Elbow, Wrist
+            joint_max_accs: Acceleration limits for each joint. List of 4 values - Base Joint, Lift, Elbow, Wrist
+            base_max_vel: Velocity limit for the base. Single float.
+            base_max_acc: Acceleration limit for the base. Single float. 
+        """
     # TODO implement joint angle limits in the config
     # TODO have a robot config.yaml and load it in backend and frontend for easy config
 
-    def get_joint_positions(self):
+    def get_joint_positions(self) -> tuple[list[float], list[float], list[float], list[float], list[float]]:
         base = self.base_position
         vertical_lift = [self.base_position[0], self.base_position[1], self.joint_states[Joints.LIFT.value]]
         elbow = [
@@ -61,27 +69,43 @@ class Robot:
 
         return base, vertical_lift, elbow, wrist, ee
 
-    def get_arm_joint_positions(self):
+    def get_arm_joint_positions(self) -> tuple[list[float], list[float], list[float]]:
+        """Returns elbow, wrist, and end effector joint positions in XYZ cartesian coordinates."""
         *_, elbow, wrist, ee = self.get_joint_positions()
         return elbow, wrist, ee
 
-    def get_ee_position(self):
+    def get_ee_position(self) -> list[float]:
+        """Returns end effector joint position in XYZ cartesian coordinates."""
         # Get ee position relative to the base
         *_, ee = self.get_arm_joint_positions()
         ee = [np.around(val, 5) for val in ee]
         return ee
 
-    def get_base_position(self):
+    def get_base_position(self) -> list[float]:
+        """Returns mobile base position in XYZ cartesian coordinates."""
         return self.base_position
 
-    def get_joint_states(self):
+    def get_joint_states(self) -> list[float]:
+        """Returns joint states - [base_joint, lift_joint, elbow_joint, wrist_joint]."""
         return self.joint_states
 
-    def get_robot_state(self):
+    def get_robot_state(self) -> tuple[list[float], list[float]]:
+        """Executes any existing generated trajectories for the base and arm joints.
+        Returns current base position and joint states.
+        """
         self.execute_traj()
         return self.get_base_position(), self.get_joint_states()
 
-    def generate_trajectory(self, target_joint_states, control_type='fk'):
+    def generate_trajectory(self, target_joint_states, control_type='fk') -> list[list[float]]:
+        """Generates trajectories in joint space for each joint.
+
+        The trajectories by default are configured to respect any supplied joint velocity and acceleration limits.
+
+        Args:
+            target_joint_states: The joint targets, must be in the order: [base, list, elbow, wrist]
+            control_type: The trajectory type, either fk (joint space) or ik (cartesian space)
+        """
+        ## TODO remove control_type
         traj = []
         max_traj_length = 0
         for joint_idx, target_joint_state in enumerate(target_joint_states):
@@ -104,8 +128,22 @@ class Robot:
         self.trajectory_points["joints"] = traj
         return traj
 
-    def generate_base_traj(self, target_position, ee_target_position=None, ee_target_orientation=None):
+    def generate_base_traj(self,
+                           target_position,
+                           ee_target_position=None,
+                           ee_target_orientation=None) -> tuple[list[tuple[float]], list[list[float]]]:
+        """Generates an X-Y trajectory for the base.
+
+        The end effector position can be maintained by sending the current position and orientation as arguments.
+        Each trajectory point of the base will then be used to solve the IK to get the joint angles needed for the EE point.
+
+        Args:
+            target_position: Target position to reach for the base.
+            ee_target_position: [Optional] Target position for the end effector during the base motion.
+            ee_target_orientation: [Optional] Target orientation for the end effector during the base motion.
+        """
         # Assuming base only moves on x,y for simplicity
+        # TODO add orientation for the base
         self.trajectory_generator.setup_trajectory(waypoints=(self.base_position[0], target_position[0]),
                                                    vel_limit=self.base_max_vel,
                                                    acc_limit=self.base_max_acc)
@@ -116,23 +154,27 @@ class Robot:
                                                    acc_limit=self.base_max_acc)
         t, y_traj = self.trajectory_generator.solve_traj(respect_limits=True)
 
+        # pad base traj with 0s assuming it only moves in X-Y plane
         z_traj = [0.0] * len(y_traj)
 
-        traj = list(zip(x_traj, y_traj, z_traj))
-        instant_traj = []
+        base_traj = list(zip(x_traj, y_traj, z_traj))
+        instant_ee_traj = []
         if ee_target_position:
             # For each base position, calculate the joint angles using IK to maintain EE position
             # For simplicity using instantaneous points
-            for base_pos in traj:
+            for base_pos in base_traj:
                 base_angle, elbow_angle, wrist_angle = self.calculate_inverse_kinematics(ee_target_position[0:2], ee_target_orientation, base_position=base_pos)
                 lift_state = ee_target_position[2] + self.wrist_vertical_offset + self.gripper_vertical_offset
                 instant_joint_targets = [base_angle, lift_state, elbow_angle, wrist_angle]
-                instant_traj.append(instant_joint_targets)
+                instant_ee_traj.append(instant_joint_targets)
 
-        self.trajectory_points["base"] = traj
-        self.trajectory_points["joints"] = instant_traj
+        self.trajectory_points["base"] = base_traj
+        self.trajectory_points["joints"] = instant_ee_traj
 
-    def execute_traj(self):
+        return base_traj, instant_ee_traj
+
+    def execute_traj(self) -> None:
+        """Executes the generated trajectories for the base and joints."""
         base_traj_points = self.trajectory_points["base"]
         joint_traj_points = self.trajectory_points["joints"]
         if len(base_traj_points) > 0:
@@ -140,7 +182,17 @@ class Robot:
         if len(joint_traj_points) > 0:
             self.joint_states = joint_traj_points.pop(0)
 
-    def calculate_inverse_kinematics(self, target_position: tuple[float, float], target_orientation: float=None, base_position=None):
+    def calculate_inverse_kinematics(self,
+                                     target_position: tuple[float, float],
+                                     target_orientation: float=None,
+                                     base_position=None) -> tuple[float, float, float]:
+        """Solves the inverse kinematics to get the joint angles for the XYZ cartesian end effector target.
+
+        The IK is solved by decoupling the kinematic chain into the base and elbow, and then the wrist. By doing so,
+        the kinematic chain can be simplified. First, the wrist centre point is calculated through the IK solution for
+        a simple 2-link arm, with the target being adjusted to account for the length and desired orientation of the ee.
+        Finally, the wrist orientation is set to the desired orientation, completing the kinematic chain from base to ee.
+        """
         # Check target validity
         if base_position is None:
             base_position = self.base_position[0:2]
@@ -180,7 +232,13 @@ class Robot:
         # base, elbow, wrist
         return theta_1, theta_2, theta_3
 
-    def move_forward_kinematics(self, target_joint_states, animate=False):
+    def move_forward_kinematics(self, target_joint_states, animate=False) -> None:
+        """Moves the robot using joint space targets.
+
+        Args:
+            target_joint_states: The joint space targets for the robot.
+            animate: Boolean dictating whether to generate a trajectory or not. Set to true for Web UI.
+        """
         if len(target_joint_states) > self.n_joints:
             print(f"Error. Too many joint states ({len(target_joint_states)} for robot with {self.n_joints} joints")
             return
@@ -191,7 +249,18 @@ class Robot:
         else:
             self.generate_trajectory(target_joint_states, 'fk')
 
-    def move_inverse_kinematics(self, target_position: tuple[float, float, float], target_orientation: float=None, animate=False):
+    def move_inverse_kinematics(self,
+                                target_position: tuple[float, float, float],
+                                target_orientation: float=None,
+                                animate=False) -> None:
+        """Moves the robot using XYZ cartesian space targets.
+
+        Args:
+            target_position: The cartesian space target for the robot.
+            target_orientation: The desired orientation angle for the end effector. 0 = approach from left, pi/2 = approach from below...
+            animate: Boolean dictating whether to generate a trajectory or not. Set to true for Web UI.
+
+        """
         # For IK trajectory simplicity, find joint angles at start and end, then generate polynomial traj between points in joint
         # space. Quicker than generating polynomial traj in cartesian space and calling IK on each point
         base_angle, elbow_angle, wrist_angle = self.calculate_inverse_kinematics(target_position[0:2], target_orientation)
@@ -205,8 +274,16 @@ class Robot:
             fk_target = [base_angle, lift_state, elbow_angle, wrist_angle]
             self.generate_trajectory(fk_target, 'fk')
 
-    def move_base(self, new_position: tuple[float, float], maintain_ee: bool=False, animate=False):
-        # Assume base only moves in a planar motion
+    def move_base(self, new_position: tuple[float, float], maintain_ee: bool=False, animate=False) -> None:
+        """Moves the base using XYZ cartesian space targets.
+
+        Base motion assumes motion only in planar X-Y plane.
+
+        Args:
+            new_position: Cartesian space target for the mobile base.
+            maintain_ee: Boolean dictating whether to maintain the current end effector position.
+            animate: Boolean dictating whether to generate a trajectory or not. Set to true for Web UI.
+        """
         # For simplicity, the base has no orientation and thus can move in any direction without turning
         # TODO need to check base final position leaves ee in range of target pose
         if animate:
@@ -221,22 +298,14 @@ class Robot:
             self.base_position[1] = new_position[1]
             self.base_position[2] = 0.0
 
-    def actuate_gripper(self, desired_state: int):
+    def actuate_gripper(self, desired_state: int) -> None:
+        """Actuate the gripper."""
         # 0 = open
         # 1 = closed
         self.joint_states[-1] = desired_state
 
-    def check_lengths(self):
-        elbow, wrist, ee = self.get_arm_joint_positions()
-        base_length = np.linalg.norm(np.subtract(self.base_position[0:2], elbow[0:2]))
-        elbow_length = np.linalg.norm(np.subtract(elbow[0:2], wrist[0:2]))
-        wrist_length = np.linalg.norm(np.subtract(wrist[0:2], ee[0:2]))
-
-        print(f"Base length is: {base_length}")
-        print(f"elbow length is: {elbow_length}")
-        print(f"wrist length is: {wrist_length}")
-
-    def plot_state(self):
+    def plot_state(self) -> None:
+        """Plot the current robot state in 2D top down view for quick logic testing."""
         fig, ax = plt.subplots()
         base = plt.Circle((self.base_position[0], self.base_position[1]), 0.1)
         ax.add_artist(base)
@@ -246,8 +315,6 @@ class Robot:
         # print(f"J2 position (Elbow): {elbow}")
         # print(f"J3 position (Wrist): {wrist}")
         # print(f"J4 position (End Effector): {ee}")
-
-        self.check_lengths()
 
         plt.plot([self.base_position[0], elbow[0]], [self.base_position[1], elbow[1]], 'bo-', label='Base Offset (L1)')  # L1
         plt.plot([elbow[0], wrist[0]], [elbow[1], wrist[1]], 'go-', label='Elbow (L2)')  # L2
